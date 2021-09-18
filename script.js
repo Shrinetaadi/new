4c28943c9625c0c3f3f0565655dfa1ebaa51fdcd
Aditya Singh <aditya.singh.953@gmail.com>
2021-09-18 09:26:29 +0530

    AA

---
+++ script.js
@@ -0,0 +1,8 @@
+var a = prompt("Enter UserName");
+var b = prompt("Enter password");
+if (a == "ankit" && b == "1234") {
+  window.location.href = "start.html";
+} else {
+  alert("invalid details");
+  window.location.href = "index.html";
+}
